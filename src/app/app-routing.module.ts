292c1afcbcdbeac71c77f7aeeb9f797580829741
Mariano Alvarez <malvarezhcr@gmail.com>
2021-02-07 21:37:58 -0600

    feat: tercer ejemplo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'ejemplo-1', loadChildren: () => import('./ejemplo1/ejemplo1.module').then(m => m.Ejemplo1Module) }
+  { path: 'ejemplo-1', loadChildren: () => import('./ejemplo1/ejemplo1.module').then(m => m.Ejemplo1Module) },
+  { path: 'ejemplo-2', loadChildren: () => import('./ejemplo2/ejemplo2.module').then(m => m.Ejemplo2Module) },
+  { path: 'ejemplo-3', loadChildren: () => import('./ejemplo3/ejemplo3.module').then(m => m.Ejemplo3Module) },
 ];
 
 @NgModule({
